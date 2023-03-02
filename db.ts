c62579768885329b3e27f59fb6db6e6e21c170be
Guksu <yung3152@daum.net>
2023-03-02 14:25:17 +0900

    배포환경 변경

--- db.ts
+++ db.ts
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createConnection({
-  user: "wenote-user",
-  host: "srv-captain--tqenzhryou-mysql-80x",
+  user: "root",
+  host: "svc.sel3.cloudtype.app",
   password: process.env.DB_PASSWORD,
-  database: "wenote-database",
-  port: 3306,
+  database: "wenote",
+  port: 32317,
 });

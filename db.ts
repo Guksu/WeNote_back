9f9a507822896da92046156caf428273ad7c5821
Guksu <yung3152@daum.net>
2023-03-02 12:32:09 +0900

    db 수정

--- db.ts
+++ db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const db = mysql.createConnection({
   user: "wenote-user",
-  host: "srv-captain--zpjnuiysrk-mysql-80x",
+  host: "srv-captain--tqenzhryou-mysql-80x",
   password: process.env.DB_PASSWORD,
   database: "wenote-database",
   port: 3306,

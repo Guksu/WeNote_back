0b12e9a3eaae32fedd2f3f460257cfd9218c2c7e
Guksu <yung3152@daum.net>
2023-03-02 18:47:42 +0900

    배포용 오류 수정

--- middleware/deploy_memId.ts
+++ middleware/deploy_memId.ts
@@ -2,9 +2,11 @@ import { Request, Response, NextFunction } from "express";
 
 const deploy_memIdMiddleWare = (req: Request, res: Response, next: NextFunction) => {
   if (req.query.memId) {
-    req.memId = +req.query.memId;
+    req.memId = Number(req.query.memId);
+    next();
+  } else {
+    next();
   }
-  next();
 };
 
 export default deploy_memIdMiddleWare;
